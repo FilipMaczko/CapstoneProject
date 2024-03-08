b9c080a44585aee531120ceb3a0cc64b761e8aee
Filip Maczko <filipmaczko1@gmail.com>
2024-03-08 15:02:48 +0100

    Even more css

--- src/Testimonial.js
+++ src/Testimonial.js
@@ -1,8 +1,10 @@
+import './Testimonial.css';
+
 function Testimonial() {
     return (
       <div className = "testimonial">
       <h1 className="cardTitle">Rating</h1>
-      <img src = './images/personaA.jpg' alt = 'persona'/>
+      <img src = 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt = 'persona'/>
       <h2 className='highlighText'>User name</h2>
       <p className= 'paragraphText'>Review text. </p>
       </div>
