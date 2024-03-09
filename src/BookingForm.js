3e4f06d9b45c7a892b399de2ce8fd72da472e09e
Filip Maczko <filipmaczko1@gmail.com>
2024-03-09 12:44:48 +0100

    Statefull form

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -1,84 +1,161 @@
-import React, {useEffect} from "react";
-import { useFormik } from "formik";
-import * as Yup from 'yup';
-import useSubmit from "./hooks/useSubmit";
-import {useAlertContext} from "./context/alertContext";
+import React, { useState } from 'react';
 
 
 const BookingForm = () => {
-  return (
-          <form>
-              <div className= 'formControl'>
-                <label htmlFor="firstName">Full Name</label>
-                <input
-                  id="fullName"
-                  name="fullName"
-                />
-                <p className="errorMessage"></p>
-              </div>
+  const [aviableHours, setAviableHours] = useState([
+    "17.00",
+    "18.00",
+    "19.00",
+    "20.00"
+  ]);
 
-              <div className= 'formControl'>
-                <label htmlFor="phoneNumber">Phone number</label>
-                <input
-                  id="phoneNumber"
-                  name="phoneNumber"
-                />
-                <p className="errorMessage"></p>
-              </div>
+  const hourOptions = aviableHours.map( x => <option>{x}</option>);
+  
+  const [formData, setFormData] = useState({
+      fullName: '',
+      phoneNumber: '',
+      reservationTime: '17:00',
+      reservationDate: '',
+      guestsCount: 1
+    });
 
-              <div className= 'formControl'>
-                <label htmlFor="reservationDate">Reservation date</label>
-                <input
-                  type= "datetime-local"
-                  id="reservationDate"
-                  name="reservationDate"
-                />
-                <p className="errorMessage"></p>
-              </div>
+    const [formErrors, setFormErrors] = useState({
+      nameError: '',
+      phoneError: '',
+      dateError: ''
+    });
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({
+        ...formData,
+        [name]: value
+      });
+    };
 
-              <div className= 'formControl'>
-                <label htmlFor="guestsCount">Number of guests</label>
-                <input
-                  type= "number"
-                  id="guestsCount"
-                  name="guestsCount"
-                />
-                <p className="errorMessage"></p>
-              </div>
-
-              <div className= 'formControl'>
-                <label htmlFor="smoker">Tabel for a smoker </label>
-                <input
-                  type= "checkbox"
-                  id="smoker"
-                  name="smoker"
-                />
-                <p className="errorMessage"></p>
-              </div>
-
-              <div className= 'formControl'>
-                <label htmlFor="pets">Will you brint pets with you </label>
-                <input
-                  type= "checkbox"
-                  id="pets"
-                  name="pets"
-                />
-                <p className="errorMessage"></p>
-              </div>
-
-              <div className= 'formControl'>
-                <label htmlFor="firstName">Notes</label>
-                <textarea
-                  id="notes"
-                  name="notes"
-                />
-                <p className="errorMessage"></p>
-              </div>
-              <button type="submit">
-                {"Submit"}
-              </button>
-          </form>
-  );
+    const validateData = (e) => 
+    {
+      const { name, value } = e.target;
+      if(name==='fullName'){
+        if(value.length < 3)
+        {
+          setFormErrors({
+            ...formErrors,
+            nameError: `Please enter your full name (first name and last name). It must contain at least 3 characters.`
+          });
+        }
+        else{
+          setFormErrors({
+            ...formErrors,
+            nameError: ``
+          });
+        }
+      }
+      else if(name==='phoneNumber'){
+        if(value.length < 9)
+        {
+          setFormErrors({
+            ...formErrors,
+            phoneError: `Phone number must contain at least 9 digits.`
+          });
+        }
+        else{
+          setFormErrors({
+            ...formErrors,
+            phoneError: ``
+          });
+        }
+      }
+      else if(name==='reservationDate'){
+        if(Date.parse(value)-Date.parse(new Date())<0)
+        {
+          setFormErrors({
+            ...formErrors,
+            dateError: `You can not select ${value}. This date is in the past.`
+          });
+        }
+        else{
+          setFormErrors({
+            ...formErrors,
+            dateError: ``
+          });
+        }
+      }
+    }
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      // Here you can submit the form data
+      console.log(formData);
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className='formControl'>
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            id="fullName"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            onBlur={validateData}
+          />
+          <p className="errorMessage">{formErrors.nameError}</p>
+        </div>
+  
+        <div className='formControl'>
+          <label htmlFor="phoneNumber">Phone number</label>
+          <input
+            type="tel"
+            id="phoneNumber"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+            onBlur={validateData}
+          />
+          <p className="errorMessage">{formErrors.phoneError}</p>
+        </div>
+  
+        <div className='formControl'>
+          <label htmlFor="reservationTime">Reservation time</label>
+          <select id="reservationTime" name="reservationTime" value={formData.reservationTime} onChange={handleChange}>
+            {hourOptions}
+          </select>
+          <p className="errorMessage"></p>
+        </div>
+  
+        <div className='formControl'>
+          <label htmlFor="reservationDate">Reservation date</label>
+          <input
+            type="datetime-local"
+            id="reservationDate"
+            name="reservationDate"
+            value={formData.reservationDate}
+            onChange={handleChange}
+            onBlur={validateData}
+          />
+          <p className="errorMessage">{formErrors.dateError}</p>
+        </div>
+  
+        <div className='formControl'>
+          <label htmlFor="guestsCount">Number of guests</label>
+          <input
+            type="number"
+            placeholder="1"
+            min="1"
+            max="10"
+            id="guestsCount"
+            name="guestsCount"
+            value={formData.guestsCount}
+            onChange={handleChange}
+          />
+          <p className="errorMessage"></p>
+        </div>
+        <button type="submit">
+          {"Submit"}
+        </button>
+      </form>
+    );
 };
 
 export default BookingForm;
